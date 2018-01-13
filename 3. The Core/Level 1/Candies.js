14b20ac90fc9134a48f291017e8ff3b5d1233a3a
Russ Wilkie <russwilkie@gmail.com>
2018-01-13 16:47:14 -0600

    Update Candies.js

--- 3. The Core/Level 1/Candies.js
+++ 3. The Core/Level 1/Candies.js
@@ -1,11 +1,14 @@
 /*
-Given an integer n, return the largest number that contains exactly n digits.
+n children have got m pieces of candy. They want to eat as much candy as they can, but each child must eat exactly the same amount of candy as any other child. Determine how many pieces of candy will be eaten by all the children together. Individual pieces of candy cannot be split.
 
 Example
 
-For n = 2, the output should be
-largestNumber(n) = 99.
+For n = 3 and m = 10, the output should be
+candies(n, m) = 9.
+
+Each child will eat 3 pieces. So the answer is 9.
 */
-function largestNumber(n) {
-    return Math.pow(10,n)-1
+function candies(n, m) {
+    return m - (m%n);
 }
+
